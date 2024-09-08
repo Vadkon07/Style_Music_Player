812c6083a7e8fc64942aac8a00310a189e056ba5
Vadkon07 <vadkonbeats@gmail.com>
2024-09-08 19:21:10 +0000

    big update

--- main.py
+++ main.py
@@ -1,89 +1,205 @@
+import sys
 import os
-from tkinter import *
-from tkinter import filedialog
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QSlider, QListWidget, QLabel, QFileDialog, QDialog
+from PyQt6.QtCore import Qt, QTimer
+from PyQt6.QtGui import QPixmap, QColor
 from pygame import mixer
-from tkinter import ttk
 
-root = Tk()
-root.geometry("480x250")
-root.title("Simple Music Player")
-root.config(bg='#000000')
-root.resizable(False, False)
-mixer.init()
+class MusicPlayer(QWidget):
+    def __init__(self):
+        super().__init__()
 
-lbl = Label(root)
-lbl.place(x=0, y=0)
+        self.IsPaused = False
 
-def update(ind):
-   frame = frms[ind]
-   ind += 1
-   if ind == frmcount:
-       ind = 0
-   lbl.config(image=frame)
-   root.after(40, update, ind)
+        self.setWindowTitle('Style Player')
+        self.setGeometry(100, 100, 480, 250)
+        self.setStyleSheet('background-color: #000000; color: lime')
+        self.setFixedSize(480, 250)
 
-def addMusic():
-   path = filedialog.askdirectory()
-   if path:
-       os.chdir(path)
-       songs = os.listdir(path)
+        mixer.init()
+        layout = QVBoxLayout()
 
-       for song in songs:
-           if song.endswith(".mp3"):
-               Playlist.insert(END, song)
+        self.lbl = QLabel(self)
+        layout.addWidget(self.lbl)
 
-def playMusic():
-    global current_song_path
-    music_name = Playlist.get(ACTIVE)
-    current_song_path = os.path.join(os.getcwd(), music_name)
-    print(music_name[0:-4])
-    mixer.music.load(current_song_path)
-    mixer.music.play()
+        self.playlist = QListWidget(self)
+        self.playlist.setStyleSheet('background-color: #000000; color: #00ff00; font: bold 12px;')
+        layout.addWidget(self.playlist)
 
-def set_volume(val):
-    volume = float(val) / 100
-    mixer.music.set_volume(volume)
+        self.play_button = QPushButton('Play', self)
+        self.play_button.setStyleSheet("""
+        QPushButton {
+            border: 1px solid;
+            border-color: lime; 
+            margin: 1px;
+            height: 25px;
+            width: 25px;
+        }
+        QPushButton:focus {
+            margin: 0px;
+        }
+        """)
 
-def update_slider():
-    if mixer.music.get_busy():
-        pos = mixer.music.get_pos() / 1000
-        length = mixer.music.get_length()
-        position_slider.set((pos / length) * 100)
-    root.after(1000, update_slider)
+        self.play_button.clicked.connect(self.play_music)
+        layout.addWidget(self.play_button)
 
+        self.toggle_button = QPushButton('Pause/Unpause', self)
+        self.toggle_button.setStyleSheet("""
+        QPushButton {
+            border: 1px solid;
+            border-color: lime; 
+            margin: 1px;
+            height: 25px;
+            width: 25px;
+        }
+        QPushButton:focus {
+            margin: 0px;
+        }
+        """)
+        self.toggle_button.clicked.connect(self.toggleMusic)
+        layout.addWidget(self.toggle_button)
 
-frm_music = Frame(root, bd=2, relief=RIDGE, width=485, height=100)
-frm_music.place(x=0, y=10)
+        self.add_button = QPushButton('Add directory', self)
+        self.add_button.setStyleSheet("""
+        QPushButton {
+            border: 1px solid;
+            border-color: lime; 
+            margin: 1px;
+            height: 25px;
+            width: 25px;
+        }
+        QPushButton:focus {
+            margin: 0px;
+        }
+        """)
+        self.add_button.clicked.connect(self.add_music)
+        layout.addWidget(self.add_button)
 
-btn_p = Button(root, text="Play", fg="White", bg='#0f0f0f', height=2, width=10, command=playMusic)
-btn_p.place(x=40, y=130)
+        self.volume_slider = QSlider(Qt.Orientation.Horizontal, self)
+        self.volume_slider.setRange(0, 100)
+        self.volume_slider.setValue(50)
+        self.volume_slider.setStyleSheet("""
+            QSlider::groove:horizontal {
+                border: 1px solid #999999;
+                height: 8px;
+                background: green;
+            }
+            QSlider::handle:horizontal {
+                background: lime;
+                border: 1px solid #999999;
+                width: 18px;
+                margin: -2px 0;
+                border-radius: 3px;
+            }
+        """)
 
-btn_s = Button(root, text="Stop", fg="White", bg='#0f0f0f', height=2, width=10, command=mixer.music.stop)
-btn_s.place(x=130, y=130)
+        self.volume_slider.valueChanged.connect(self.set_volume)
+        layout.addWidget(self.volume_slider)
 
-btn_v = Button(root, text="Unpause", fg="White", bg='#0f0f0f', height=2, width=10, command=mixer.music.unpause)
-btn_v.place(x=210, y=130)
+        self.slider = QSlider(Qt.Orientation.Horizontal, self)
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(100)
+        self.slider.setValue(0)
+        self.slider.setTickPosition(QSlider.TickPosition.TicksBelow)
+        self.slider.setTickInterval(10)
+        self.slider.sliderMoved.connect(self.set_position)  
 
-btn_ps = Button(root, text="Pause", fg="White", bg='#0f0f0f', height=2, width=10, command=mixer.music.pause)
-btn_ps.place(x=300, y=130)
+        self.setLayout(layout)
 
-btn_browse = Button(root, text="Browse Music", font=('Arial,bold', 14), fg="White", bg="#000000", width=45, command=addMusic)
-btn_browse.place(x=0, y=210)
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_slider)
+        self.timer.start(1000)
 
-volume_slider = ttk.Scale(root, from_=100, to=0, orient='vertical', command=set_volume)
-volume_slider.set(50)
-volume_slider.pack(pady=50)
-volume_slider.place(x=450, y=90)
+        self.is_slider_moving = False
 
-Scroll = Scrollbar(frm_music)
-Playlist = Listbox(frm_music, width=100, font=('Arial,bold', 12), bg='#000000', fg='#00ff00', selectbackground="lightblue", cursor="hand2", bd=0, yscrollcommand=Scroll.set)
-Scroll.config(command=Playlist.yview)
-Scroll.pack(side=RIGHT, fill=Y)
-Playlist.pack(side=RIGHT, fill=BOTH)
+    def exit_app(self):
+        self.close()
 
-#mixer.music.play(-1)
-update_slider()
+    def toggleMusic(self):
+        if self.IsPaused:
+            mixer.music.unpause()
+            self.IsPaused = False
+        else:
+            mixer.music.pause()
+            self.IsPaused = True
 
-root.after(0, update, 0)
-root.mainloop()
+    def update(self, ind):
+        frame = self.frms[ind]
+        ind += 1
+        if ind == self.frmcount:
+            ind = 0
+        self.lbl.setPixmap(QPixmap(frame))
+        QTimer.singleShot(40, lambda: self.update(ind))
 
+    def add_music(self):
+        dialog = QFileDialog(self)
+        dialog.setOption(QFileDialog.Option.DontUseNativeDialog, True)
+        dialog.setStyleSheet("""
+            QDialog {
+                background-color: black;
+                color: lime;
+            }
+            QLabel, QLineEdit, QPushButton {
+                color: lime;
+                background-color: black;
+            }
+            QListView, QTreeView {
+                background-color: black;
+                color: lime;
+            }
+            QHeaderView::section {
+                background-color: black;
+                color: lime;
+            }
+            QScrollBar:vertical {
+                background-color: black;
+            }
+            QScrollBar::handle:vertical {
+                background-color: lime;
+            }
+        """)
+
+        path = dialog.getExistingDirectory(self, "Select Music Folder")
+        if path:
+            try:
+                songs = os.listdir(path)
+                for song in songs:
+                    if song.endswith(".mp3"):
+                        self.playlist.addItem(os.path.join(path, song))
+            except Exception as e:
+                print(f"An error occurred: {e}")
+
+    def set_position(self, position):
+        self.is_slider_moving = True
+        mixer.music.play(start=position)
+        self.is_slider_moving = False
+
+    def play_music(self):
+        global current_song_path
+        current_item = self.playlist.currentItem()
+        if current_item is not None:
+            music_name = current_item.text()
+            current_song_path = os.path.join(os.getcwd(), music_name)
+            print(music_name[:-4])
+            mixer.music.load(current_song_path)
+            mixer.music.play()
+        else:
+            print("No item selected")
+
+            track_length = mixer.Sound(current_song_path).get_length()
+            self.slider.setMaximum(int(track_length))
+
+    def set_volume(self, val):
+        volume = float(val) / 100
+        mixer.music.set_volume(volume)
+
+    def update_slider(self):
+        if mixer.music.get_busy():
+            current_position = mixer.music.get_pos() / 1000 
+            self.slider.setValue(int(current_position))
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    player = MusicPlayer()
+    player.show()
+    sys.exit(app.exec())
